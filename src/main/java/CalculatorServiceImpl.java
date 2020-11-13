0b32df017e2ffb3dd6430c2b94343b139b408f19
dasem <gfhflbuvf46@gmail.com>
2020-11-13 13:03:00 +0500

    some improvements

--- src/main/java/CalculatorServiceImpl.java
+++ src/main/java/CalculatorServiceImpl.java
@@ -2,12 +2,15 @@ import com.google.protobuf.Timestamp;
 import com.trrp.trrp3server.*;
 import io.grpc.stub.StreamObserver;
 
+import java.text.MessageFormat;
 import java.util.Date;
 
 public class CalculatorServiceImpl extends CalculatorGrpc.CalculatorImplBase {
     @Override
     public void calculate(CalculateMessage request, StreamObserver<CalculatedMessage> responseObserver) {
         CalculatedMessage.Builder calculatedBuilder = CalculatedMessage.newBuilder();
+        calculatedBuilder.setCalculationStatus(CalculationStatus.OK.name());
+
         Date calculationStart = new Date();
 
         double op1 = request.getOperand1();
@@ -15,7 +18,7 @@ public class CalculatorServiceImpl extends CalculatorGrpc.CalculatorImplBase {
 
         switch (Operation.valueOf(request.getOperation())) {
             case DIV:
-                if (0 == op1) {
+                if (0 == op2) {
                     calculatedBuilder.setCalculationStatus(CalculationStatus.FAILED.name());
                 } else {
                     calculatedBuilder.setResult(op1 / op2);
@@ -34,6 +37,13 @@ public class CalculatorServiceImpl extends CalculatorGrpc.CalculatorImplBase {
 
         calculatedBuilder.setCalculationTime((new Date().getTime() - calculationStart.getTime()) / 1000.0);
         calculatedBuilder.setCalculationDate(getCurrentTimestamp());
+
+        System.out.println(MessageFormat.format("Calculating...\n" +
+                "op1: {0}\n" +
+                "op2: {1}\n" +
+                "operation: {2}\n" +
+                "result: {3}",
+                op1, op2, request.getOperation(), calculatedBuilder.getResult()));
 
         responseObserver.onNext(calculatedBuilder.build());
         responseObserver.onCompleted();
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,30 @@
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.text.MessageFormat;
+import java.util.Properties;
 
 public class Main {
 
+
+
     public static void main(String[] args) throws IOException, InterruptedException {
-        Server server = ServerBuilder.forPort(5300)
+
+        FileInputStream fileInputStream = new FileInputStream("src/main/resources/default.properties");
+        Properties property = new Properties();
+        property.load(fileInputStream);
+
+        int port = Integer.parseInt(property.getProperty("port"));
+
+        Server server = ServerBuilder.forPort(port)
                 .addService(new CalculatorServiceImpl())
                 .build();
         server.start();
+
+        System.out.println(MessageFormat.format("Server successfully started on port : {0}", String.valueOf(port)));
+
         server.awaitTermination();
     }
 }
